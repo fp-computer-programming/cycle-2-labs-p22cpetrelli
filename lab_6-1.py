d22531459e8c4e7d6eec15817e87f5c64b08bf5e
Cam Petrelli <p22cpetrelli@fairfieldprep.org>
2021-09-21 10:06:37 -0400

    add file

---
+++ lab_6-1.py
@@ -0,0 +1,8 @@
+# Author: CJP (AMDG) 9/21/21
+
+first_name = input("Please Enter First Name ")
+
+last_name = input("Please Enter Last Name ")
+
+full_name = first_name + " " + last_name
+print(full_name)
